0b19a09e18f7000cbfbe082a28c3f505e9c3df70
Brian Freitas <brianfreitas95@gmail.com>
2025-07-31 17:43:25 -0300

    Criei minha primeira migration da tabela de produto

---
+++ Pasta do fracasso/FracassoShopping/FracassoShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FracassoShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        {
+            
+        }
+
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Pasta do fracasso/FracassoShopping/FracassoShopping.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FracassoShopping.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
